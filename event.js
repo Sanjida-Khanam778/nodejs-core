3156d31b04bda8becc213d676cf9b19c4b3308b6
Sanjida Khanam <sanjida.ice.778@gmail.com>
2025-06-02 13:31:03 +0600

    event listener add

--- event.js
+++ event.js
@@ -2,8 +2,16 @@ const EventEmitter = require("node:events");
 
 class SchoolBell extends EventEmitter {}
 
-const SchoolBell = new SchoolBell();
+SchoolBell = new SchoolBell();
 SchoolBell.on("ring", () => {
   console.log("an event occurred!");
 });
+SchoolBell.on("ring", () => {
+  console.log("Another event occurred!");
+});
+SchoolBell.on("Broken", () => {
+  console.log("New event occurred!");
+});
+
 SchoolBell.emit("ring");
+SchoolBell.emit("Broken");
