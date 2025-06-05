0058e779841492dc4d123a8f0cd5bad217df7811
Sanjida Khanam <sanjida.ice.778@gmail.com>
2025-06-05 09:45:23 +0600

    read a file and write on a file synchronously

---
+++ fs.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+const text = 'Learning File System'
+
+fs.writeFileSync('./input.txt', text)
+const data = fs.readFileSync("./input.txt", { encoding: "utf-8" });
+
+console.log(data);
